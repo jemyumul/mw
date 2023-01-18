1818da1f0168494db395ed29a2a935f0213ba071
Jem Yumul <10107293+jemyumul@users.noreply.github.com>
2023-01-19 03:36:19 +0800

    revert to old db setup

--- db/db.js
+++ db/db.js
@@ -1,14 +1,12 @@
 const mysql = require("mysql2/promise");
 async function getConnection() {
   // create the connection
-  const connection = await mysql.createConnection(process.env.DATABASE_URL);
-  // const connection = await mysql.createConnection({
-  //   host: process.env.DATABASE_URL,
-  //   port:  3306,
-  //   user: "maxwax_test",
-  //   password: "l0rdg@b3n",
-  //   database: "maxwax_cms",
-  // });
+  const connection = await mysql.createConnection({
+    host: "198.57.224.67",
+    user: "maxwax_test",
+    password: "l0rdg@b3n",
+    database: "maxwax_cms",
+  });
   return connection;
 }
 
