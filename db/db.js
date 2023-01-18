c41cfd00fa695adfc71a52eb51cdf7e412398138
Jem Yumul <10107293+jemyumul@users.noreply.github.com>
2023-01-19 03:25:42 +0800

    process env database

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 async function getConnection() {
   // create the connection
   const connection = await mysql.createConnection({
-    host: "web.r2groupofcompanies.com",
+    host: process.env.DATABASE_URL,
     port:  3306,
     user: "maxwax_test",
     password: "l0rdg@b3n",
