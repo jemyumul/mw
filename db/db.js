dfecc734d5188ea0a767d4237a06f6ad1c93fe23
Jem Yumul <10107293+jemyumul@users.noreply.github.com>
2023-01-19 05:54:58 +0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,8 +1,16 @@
 const mysql = require("mysql2/promise");
-const parseDbUrl = require("parse-database-url")
+const parseDbUrl = require("parse-database-url");
 
 async function getConnection() {
   // create the connection
+  // const connection = await mysql.createConnection({
+  //   host: "web.r2groupofcompanies.com",
+  //   port:  3306,
+  //   user: "maxwax_test",
+  //   password: "l0rdg@b3n",
+  //   database: "maxwax_cms",
+  // });
+
   const dbConfig = parseDbUrl(process.env.DATABASE_URL);
   const connection = await mysql.createConnection(dbConfig);
   return connection;
